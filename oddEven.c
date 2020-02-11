bf07d25febb899859b63f2b53dd4141a9c2bf71e
Ramya-V <rajaramya559@gmail.com>
2020-02-11 23:21:43 +0530

    odd or even number

---
+++ oddEven.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main()
+{
+
+	int n;
+	printf("Enter the number: ");
+	scanf("%d",&n);
+	
+	if(n%2==0)
+	{
+		printf("It is a even number");
+	}
+	else
+	{
+		printf("It is an odd number");
+	}
+}
+
